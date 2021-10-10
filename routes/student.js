29cc7aaa55781f7898260f700b5ee1039b65ce8f
Abhay Patidar <patidarabhayn1@gmail.com>
2021-10-11 00:22:58 +0530

    Student Update By His/Her Mentor Only

--- routes/student.js
+++ routes/student.js
@@ -1,11 +1,84 @@
 var express = require('express');
 const Student = require('../models/student');
+const StudentBatch = require('../models/studentBatch');
+const Mentoring = require('../models/mentoringRecord');
 const bodyParser = require('body-parser')
 var passport = require('passport');
 var authenticate = require('../authenticate');
 
 var router = express.Router();
 router.use(bodyParser.json());
+
+router.route('/:studentId')
+.get(authenticate.verifyTeacher, (req, res, next) => {
+  Student.findById(req.params.studentId)
+  .then((student) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(student);
+  }, (err) => next(err))
+  .catch((err) => {
+    next(err);
+  })
+})
+
+.put(authenticate.verifyTeacher, (req, res, next) => {
+  Student.findById(req.params.studentId)
+  .then((student) => {
+      if(student != null) {
+          StudentBatch.find({mentee: student._id})
+          .then((record) => {
+            if(record.length > 0){
+              Mentoring.findById(record[0].batch)
+              .then((batch) => {
+                console.log(batch);
+                if(batch != null) {
+                  if(!batch.mentor.equals(req.user._id)){
+                    var err = new Error('You are not authorised to update this student');
+                    err.status= 403;
+                    return next(err);
+                  }
+                  else{    
+                    Student.findByIdAndUpdate(req.params.studentId, {
+                        $set: req.body
+                    }, {
+                        new: true
+                    })
+                    .then((student) => {
+                        res.statusCode = 200;
+                        res.setHeader('Content-Type','application/json');
+                        res.json(student);
+                    }, (err) => next(err))
+                    .catch((err) => {
+                        next(err);
+                    })
+                }
+                }
+                else {
+                  var err = new Error('Mentor Not Exist');
+                  err.status= 403;
+                  return next(err);
+                }
+              })
+            }
+            else {
+              var err = new Error('No Mentor Record for this Student');
+              err.status= 403;
+              return next(err);
+            }
+          })
+      }
+      else {
+          var err = new Error('Student does not exist');
+          err.status= 403;
+          return next(err);
+      }
+  })
+  .catch((err) => {
+    next(err);
+  })
+})
+
 
 router.route('/profile')
 .get(authenticate.verifyStudent, (req, res, next) => {
